40609fd42a3feac9192b00a8ff684e18d4c7d135
Roman Adamov <shkiperko0@gmail.com>
2026-07-18 17:21:46 +0600

    imp

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,51 @@
 package main
 
+/*
+	Блок импорта внешних зависимостей или стандартных библиотек
+*/
+
 import (
 	"fmt"
 	"math"
 )
 
+/*
+	Блок создания НЕизменяемых переменных
+*/
+
 const (
-	PI           = math.Pi
-	GREET string = "привет"
+	Pi           = math.Pi // реимпорт из другого пакета
+	Greet string = "Привет"
 )
+
+/*
+	Блок создания изменяемых переменных
+*/
 
 var (
-	a int     = 1
-	b bool    = true
-	c uint32  = 3
-	d int32   = 4
-	e rune    = 'f'
-	f float32 = 3.14
+	globalVar = 0 // переменная в глобальной области
+
+	a int     = 1    // псевдоним для int32
+	b bool    = true // истина иили лож
+	c uint32  = 3    // только положительные целые числа
+	d int32   = 4    // любые целые числа
+	e rune    = 'f'  // один символ из строки
+	f float32 = 3.14 // число с плавающей точкой одинарной точности (float64 двойной точности)
+	g byte    = 255  // минимально возможная адресуемая единица памяти 1 байт (8 бит)
 )
 
-func pickMe(god string, duck, arg3 int) (int, string) {
-
-	fmt.Println(god, duck, arg3)
-	return duck + arg3, god + " пидор"
+func myPrintln(greet string, a, b int) (int, string) {
+	fmt.Println(greet, a, b)
+	return a + b, greet + " друг"
 }
-func main() {
-	var a = 0
-	a, suca := pickMe(GREET, a, int(d))
-	fmt.Println(a, suca, float32(0.0001)+float32(0.0001))
 
+/*
+	main входная точка в программу
+	ничего не принимает и не возвращает
+*/
+
+func main() {
+	var localVar = 0 // переменная в локальной области
+	localVar, message := myPrintln(Greet, localVar, int(d))
+	fmt.Println(localVar, message)
 }
